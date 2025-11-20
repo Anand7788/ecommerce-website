3afca2ea6d6e9d3331cc1fccd37a2b326934da69
Anand7788 <anandkumarprasad750@gmail.com>
2025-11-20 07:25:05 +0000

    Configure CORS and hosts for Render deployment

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -70,6 +70,10 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  # Allow Render hostnames
+  config.hosts << /.*render.com/
+
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,10 +1,13 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # change to your frontend origin later e.g. 'http://localhost:3001'
+    origins 'http://localhost:5173',
+            'http://127.0.0.1:5173',
+            'https://shopperspoint-app.netlify.app'
+
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization']
+      expose: ['Cart-Token'],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
